8bbf5ada49cbbd340d5a7dee6325201c5c4ba00e
Hedwig Doets <hedwig.doets@mendix.com>
2022-11-30 14:29:37 +0100

    Remove generated files

--- Shared/ComponentParent/src/EngineContext.js
+++
@@ -1,8 +0,0 @@
-//These contexts are defined here and set as global, since we need to access the same consts from different widgets.
-import React from "react";
-const global = globalThis;
-export const EngineContext = React.createContext({});
-export const ParentContext = React.createContext(NaN);
-global.EngineContext = EngineContext;
-global.ParentContext = ParentContext;
-//# sourceMappingURL=EngineContext.js.map
