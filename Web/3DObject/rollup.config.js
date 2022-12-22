71044e0c451cfcaf14e7ac3a2ce06e87901e42cb
Hedwig Doets <hedwig.doets@mendix.com>
2022-12-22 15:23:52 +0100

    Add regex check to writebundle step to replace loader references

--- Web/3DObject/rollup.config.js
+++ Web/3DObject/rollup.config.js
@@ -1,47 +1,37 @@
 import typescript from "@rollup/plugin-typescript";
-const copy = require("rollup-plugin-copy");
-import commonjs from "@rollup/plugin-commonjs";
+import fs from "fs";
 
 export default args => {
     const result = args.configDefaultConfig;
     result.treeshake = false;
     result.forEach(config => {
-        const external = [/^@babylonjs\/core($|\/)/, /^@babylonjs\/loaders($|\/)/];
+        const external = [/^@babylonjs\/core($|\/)/];
         config.external = [...config.external, ...external];
-
         config.plugins.push(
             typescript({
                 include: ["../../Shared/ComponentParent/**/*.ts+(|x)", "./**/*.ts+(|x)"]
             }),
-            commonjs(),
-            copy({
-                verbose: true,
-                copyOnce: true,
-                targets: [
-                    {
-                        src: "./babylonjsloadersGLTF.js",
-                        dest: "dist/tmp/widgets/com/mendix/shared"
+            {
+                writeBundle(bundle) {
+                    for (const [fileName, chunkOrAsset] of Object.entries(bundle)) {
+                        if (
+                            chunkOrAsset !== undefined &&
+                            (chunkOrAsset.toString().endsWith(".js") || chunkOrAsset.toString().endsWith(".mjs")) &&
+                            fileName === "file"
+                        ) {
+                            const data = fs.readFileSync(chunkOrAsset, { encoding: "utf8" });
+                            const regex = /@babylonjs\/core\/.*?\.js/g;
+                            fs.writeFileSync(chunkOrAsset, data.replaceAll(regex, "../../../shared/babylonjscore"));
+                        }
                     }
-                ]
-            }),
-            copy({
-                verbose: true,
-                copyOnce: true,
-                targets: [
-                    {
-                        src: "./babylonjsloadersOBJ.js",
-                        dest: "dist/tmp/widgets/com/mendix/shared"
-                    }
-                ]
-            })
+                }
+            }
         );
         config.output.paths = {
             ...config.output.paths,
-            "@babylonjs/core": "../../../shared/babylonjscore.js",
-            "@babylonjs/loaders/glTF": "../../../shared/babylonjsloadersGLTF.js",
-            "@babylonjs/loaders/OBJ": "../../../shared/babylonjsloadersOBJ.js"
-            // "@babylonjs/loaders/STL": "../../../shared/babylonjsloaders.js"
+            "@babylonjs/core": "../../../shared/babylonjscore.js"
         };
     });
+
     return result;
 };
