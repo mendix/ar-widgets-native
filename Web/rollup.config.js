8ecc4c70810f3a6450594af6480e31d08b4add82
Hedwig Doets <hedwig.doets@mendix.com>
2023-01-18 13:08:51 +0100

    Fix origin rollup config

--- Web/rollup.config.js
+++ Web/rollup.config.js
@@ -1,3 +1,22 @@
-import defaultConfig from "../rollup.config";
+import typescript from "@rollup/plugin-typescript";
 
-export default defaultConfig;
+import commonjs from "@rollup/plugin-commonjs";
+
+export default args => {
+    const result = args.configDefaultConfig;
+    result.forEach(config => {
+        const external = [/^@babylonjs\/core($|\/)/];
+        config.external = [...config.external, ...external];
+        config.plugins.push(
+            typescript({
+                include: ["../../Shared/ComponentParent/**/*.ts+(|x)", "./**/*.ts+(|x)"]
+            }),
+            commonjs()
+        );
+        config.output.paths = {
+            ...config.output.paths,
+            "@babylonjs/core": "../../../shared/babylonjscore.js"
+        };
+    });
+    return result;
+};
