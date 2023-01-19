e8d613c27cc37718093dbaaa3159ff68726738cb
Hedwig Doets <hedwig.doets@mendix.com>
2023-01-19 15:58:05 +0100

    Add gizmo as component instead of HOC

---
+++ Shared/ComponentParent/src/GizmoComponent.tsx
@@ -0,0 +1,43 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { BoundingBoxGizmo, Color3, Mesh, Vector3 } from "@babylonjs/core";
+
+export function GizmoComponent(props: {
+    mesh?: Mesh;
+    draggingEnabled: boolean;
+    onScale: (newScale: Vector3) => void;
+}): React.ReactElement {
+    const [gizmo, setGizmo] = useState<BoundingBoxGizmo>();
+    useEffect(() => {
+        if (props.mesh !== undefined) {
+            if (gizmo === undefined && props.draggingEnabled === true) {
+                const localGizmo = new BoundingBoxGizmo();
+                localGizmo.enableDragBehavior();
+                localGizmo.onScaleBoxDragObservable.add(() => {
+                    props.onScale(props.mesh!.scaling.clone());
+                });
+                localGizmo.onScaleBoxDragEndObservable.add(() => {
+                    localGizmo.updateBoundingBox();
+                });
+
+                localGizmo.setColor(Color3.White());
+
+                localGizmo.rotationSphereSize = 0.05;
+                localGizmo.scaleBoxSize = 0.05;
+                localGizmo.attachedMesh = props.mesh;
+                setGizmo(localGizmo);
+            }
+            if (gizmo !== undefined && props.draggingEnabled === false) {
+                refreshGizmo();
+            }
+        }
+    }, [props.draggingEnabled, props.mesh, gizmo]);
+
+    const refreshGizmo = () => {
+        if (gizmo !== undefined) {
+            gizmo.dispose();
+            setGizmo(undefined);
+        }
+    };
+
+    return <Fragment />;
+}
