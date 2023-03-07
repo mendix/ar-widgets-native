f075c391adc70a8ecfb89f235b1183f9caebe1b1
Hedwig Doets <hedwig.doets@mendix.com>
2023-03-07 14:45:09 +0100

    Update native widgets to react native 0.70

--- Native/ImageTracker/typings/ARImageTrackerProps.d.ts
+++ Native/ImageTracker/typings/ARImageTrackerProps.d.ts
@@ -1,7 +1,7 @@
 /**
  * This file was generated from ARImageTracker.xml
  * WARNING: All changes made to this file will be overwritten
- * @author Mendix UI Content Team
+ * @author Mendix Widgets Framework Team
  */
 import { ComponentType, CSSProperties, ReactNode } from "react";
 import { ActionValue, DynamicValue, NativeImage } from "mendix";
