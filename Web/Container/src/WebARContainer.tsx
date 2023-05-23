8b01a8472e0700be6d05b2923638acc67fe4a6a4
hedwig.doets <hedwig.doets@mendix.com>
2023-05-23 10:20:58 +0200

    Run formatter

--- Web/Container/src/WebARContainer.tsx
+++ Web/Container/src/WebARContainer.tsx
@@ -1,10 +1,22 @@
 import { createElement, ReactElement, useEffect, useRef, useState } from "react";
 import { EngineContext, ParentContext } from "../../../Shared/ComponentParent/src/EngineContext";
 import { ValueStatus } from "mendix";
-import { ArcRotateCamera, Color3, Color4, CubeTexture, Engine, HemisphericLight, Mesh, Scene, SceneLoader, Tools, Vector3, WebXRExperienceHelper, WebXRSessionManager } from "@babylonjs/core";
+import {
+    ArcRotateCamera,
+    Color3,
+    Color4,
+    CubeTexture,
+    Engine,
+    HemisphericLight,
+    Mesh,
+    Scene,
+    SceneLoader,
+    Tools,
+    Vector3,
+    WebXRExperienceHelper,
+    WebXRSessionManager
+} from "@babylonjs/core";
 import { WebARContainerContainerProps } from "../typings/WebARContainerProps";
-// import SceneComponent from "babylonjs-hook";
-
 export function WebARContainer(props: WebARContainerContainerProps): ReactElement {
     const [scene, setScene] = useState<Scene>();
     const [parent, setParent] = useState<Mesh | undefined>();
@@ -20,72 +32,64 @@ export function WebARContainer(props: WebARContainerContainerProps): ReactElemen
     }, [scene, props.mxHdrPath]);
 
     useEffect(() => {
-        if(scene === undefined){
-        const engine = new Engine(canvasRef.current, true, {xrCompatible: true}, true);
-        function updateSize() {
-            console.log("Resize")
-            engine.resize();
-          }
-          
-        window.addEventListener('resize', updateSize);
-        const newScene = new Scene(engine);
-        
-        newScene.clearColor = new Color4(0.5,0.5,0.5,1);
-        // newScene.createDefaultEnvironment();
-        const camera = new ArcRotateCamera(
-            "MainCamera",
-            Tools.ToRadians(-90),
-            Tools.ToRadians(70),
-            Number(props.mxPreviewCameraDistance),
-            new Vector3(0, 0, 0),
-            newScene
-        );
-        camera.minZ = 0.1;
-        // This targets the camera to scene origin
-        new HemisphericLight("light", Vector3.Up(), newScene);
-        setScene(newScene);
-
-        // This attaches the camera to the canvas
-        camera.attachControl(canvasRef.current, true);
-
-        // Add a parent so scene can be rotated in preview mode.
-        const newParent = new Mesh("ContainerARParent", newScene);
-
-        setParent(newParent);
-        setParentID(newParent.uniqueId);
-        engine.runRenderLoop(function () {
-            newScene.render();
-          });
-
-        if (parent) {
-            parent.rotation = Vector3.Zero();
-        }
-
-        const instantiateWebXR = async () => {
-            const supported = await WebXRSessionManager.IsSessionSupportedAsync('immersive-vr');
-            console.log("is immersive-vr supported? " + supported)
-            var defaultXRExperience = await newScene.createDefaultXRExperienceAsync({
-                uiOptions: {
-                    sessionMode: 'immersive-vr'
-                }
-            } );
-            if(!defaultXRExperience.baseExperience){
-                console.log("No XR support")
-            } else {
-                // defaultXRExperience.baseExperience.enterXRAsync("immersive-ar", "unbounded")
-                console.log("XR supported, state: " + defaultXRExperience.baseExperience.state)
+        if (scene === undefined) {
+            const engine = new Engine(canvasRef.current, true, { xrCompatible: true }, true);
+            function updateSize() {
+                console.log("Resize");
+                engine.resize();
             }
+
+            window.addEventListener("resize", updateSize);
+            const newScene = new Scene(engine);
+
+            newScene.clearColor = new Color4(0.5, 0.5, 0.5, 1);
+            const camera = new ArcRotateCamera(
+                "MainCamera",
+                Tools.ToRadians(-90),
+                Tools.ToRadians(70),
+                Number(props.mxPreviewCameraDistance),
+                new Vector3(0, 0, 0),
+                newScene
+            );
+            camera.minZ = 0.1;
+            // This targets the camera to scene origin
+            new HemisphericLight("light", Vector3.Up(), newScene);
+            setScene(newScene);
+
+            // This attaches the camera to the canvas
+            camera.attachControl(canvasRef.current, true);
+
+            // Add a parent so scene can be rotated in preview mode.
+            const newParent = new Mesh("ContainerARParent", newScene);
+
+            setParent(newParent);
+            setParentID(newParent.uniqueId);
+            engine.runRenderLoop(function () {
+                newScene.render();
+            });
+
+            if (parent) {
+                parent.rotation = Vector3.Zero();
+            }
+
+            const instantiateWebXR = async () => {
+                const supported = await WebXRSessionManager.IsSessionSupportedAsync("immersive-vr");
+                console.log("is immersive-vr supported? " + supported);
+                var defaultXRExperience = await newScene.createDefaultXRExperienceAsync({
+                    uiOptions: {
+                        sessionMode: "immersive-vr"
+                    }
+                });
+                if (!defaultXRExperience.baseExperience) {
+                    console.log("No XR support");
+                } else {
+                    // defaultXRExperience.baseExperience.enterXRAsync("immersive-ar", "unbounded")
+                    console.log("XR supported, state: " + defaultXRExperience.baseExperience.state);
+                }
+            };
+            instantiateWebXR();
         }
-        instantiateWebXR();
-
-        
-    }
-
-    }, [canvasRef.current])
-    // const onSceneReady = async (newScene: Scene) => {
-    //     // This creates and positions a free camera (non-mesh)
-       
-    // };
+    }, [canvasRef.current]);
 
     return (
         <EngineContext.Provider
@@ -95,13 +99,7 @@ export function WebARContainer(props: WebARContainerContainerProps): ReactElemen
         >
             <div id="reader" />
             <ParentContext.Provider value={parentID}>{props.mxContentWidget}</ParentContext.Provider>
-            <canvas ref={canvasRef} style={{ width: "100%", height: "100%" }}/>
-            {/* <SceneComponent
-                antialias
-                adaptToDeviceRatio={true}
-                onSceneReady={onSceneReady}
-                style={{ width: "100%", height: "100%" }}
-            /> */}
+            <canvas ref={canvasRef} style={{ width: "100%", height: "100%" }} />
         </EngineContext.Provider>
     );
 }
--- Web/ImageTracker/src/WebARImageTracker.tsx
+++ Web/ImageTracker/src/WebARImageTracker.tsx
@@ -1,17 +1,8 @@
 import React, { createElement, Context, useState, useEffect, useRef, useContext } from "react";
-import {
-    Mesh,
-    MeshBuilder,
-    PointerEventTypes,
-    Ray,
-    Vector3,
-} from "@babylonjs/core";
+import { Mesh, MeshBuilder, PointerEventTypes, Ray, Vector3 } from "@babylonjs/core";
 import { WebARImageTrackerContainerProps } from "../typings/WebARImageTrackerProps";
 import { GlobalContext, EngineContext } from "../../../Shared/ComponentParent/typings/GlobalContextProps";
-import {
-    BrowserMultiFormatReader,
-    ResultPoint
-} from "@zxing/library/cjs";
+import { BrowserMultiFormatReader, ResultPoint } from "@zxing/library/cjs";
 
 export function WebARImageTracker(props: WebARImageTrackerContainerProps): React.ReactElement | void {
     const global = globalThis;
@@ -20,43 +11,41 @@ export function WebARImageTracker(props: WebARImageTrackerContainerProps): React
     let canvasRef = useRef<HTMLCanvasElement | null>(null);
     let codeReaderRef = useRef<BrowserMultiFormatReader>();
     let videoRef = useRef<HTMLVideoElement>();
-    
 
     const [parentID, setParentID] = useState<number>(NaN);
     const [resultsMap, setResultsMap] = useState<{ id: string; result: ResultPoint[] }[]>([]);
     const [cubes, setCubes] = useState<{ id: string; mesh: Mesh; previousRays: Ray[] }[]>([]);
     const [observableSet, setObservableSet] = useState<boolean>(false);
     const stopped = useRef<Boolean>(true);
-    
+
     const ClosestPointOnTwoLines = (ray1: Ray, ray2: Ray): Vector3 => {
         const lineVec1: Vector3 = ray1.direction;
         const lineVec2: Vector3 = ray2.direction;
         const linePoint1: Vector3 = ray1.origin;
         const linePoint2: Vector3 = ray2.origin;
-    
+
         const a = Vector3.Dot(lineVec1, lineVec1);
         const b = Vector3.Dot(lineVec1, lineVec2);
         const e = Vector3.Dot(lineVec2, lineVec2);
         const d = a * e - b * b;
         if (d !== 0.0) {
-          const r = linePoint1.subtract(linePoint2);
-          const c = Vector3.Dot(lineVec1, r);
-          const f = Vector3.Dot(lineVec2, r);
-          const s = (b * f - c * e) / d;
-          const t = (a * f - c * b) / d;
-          const resultLine1 = linePoint1.add(lineVec1.scale(s));
-          const resultLine2 = linePoint2.add(lineVec2.scale(t));
-          const result: Vector3 = resultLine1.add(resultLine2).scale(0.5);
-          return result;
+            const r = linePoint1.subtract(linePoint2);
+            const c = Vector3.Dot(lineVec1, r);
+            const f = Vector3.Dot(lineVec2, r);
+            const s = (b * f - c * e) / d;
+            const t = (a * f - c * b) / d;
+            const resultLine1 = linePoint1.add(lineVec1.scale(s));
+            const resultLine2 = linePoint2.add(lineVec2.scale(t));
+            const result: Vector3 = resultLine1.add(resultLine2).scale(0.5);
+            return result;
         } else {
-          return Vector3.Zero();
+            return Vector3.Zero();
         }
-      };
+    };
 
     useEffect(() => {
         const codeReader = new BrowserMultiFormatReader();
         codeReaderRef.current = codeReader;
-
 
         let videoElement = document.createElement("video");
         videoRef.current = videoElement;
@@ -74,9 +63,9 @@ export function WebARImageTracker(props: WebARImageTrackerContainerProps): React
     }, []);
 
     useEffect(() => {
-        if(engineContext.scene && !observableSet){
+        if (engineContext.scene && !observableSet) {
             const scan = async () => {
-                if(codeReaderRef.current && videoRef.current && stopped.current){
+                if (codeReaderRef.current && videoRef.current && stopped.current) {
                     stopped.current = false;
                     const mediaStreamConstraints: MediaStreamConstraints = {
                         audio: false,
@@ -86,69 +75,71 @@ export function WebARImageTracker(props: WebARImageTrackerContainerProps): React
                             height: { min: 720, ideal: 1080, max: 1440 }
                         }
                     };
-                    try{
-                         navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then(stream => { 
-                            if(codeReaderRef.current && videoRef.current){
-                            codeReaderRef.current.timeBetweenDecodingAttempts = 500;
-                            videoRef.current.srcObject = stream;
-                            videoRef.current.autofocus = true;
-                            videoRef.current.playsInline = true; // Fix error in Safari
-                            videoRef.current.play().then(() => {
-                                codeReaderRef.current!.decodeOnce(videoRef.current!).then(result => {
-                                    setResultsMap(oldResults => {
-                                        if (oldResults.find(old => old.id === result.getText())) {
-                                            const changedResult = oldResults.slice(0);
-                                            let indexToChange: number | undefined;
-                                            changedResult.forEach((element, index) => {
-                                                if (element.id === result.getText()) {
-                                                    indexToChange = index;
+                    try {
+                        navigator.mediaDevices
+                            .getUserMedia(mediaStreamConstraints)
+                            .then(stream => {
+                                if (codeReaderRef.current && videoRef.current) {
+                                    codeReaderRef.current.timeBetweenDecodingAttempts = 500;
+                                    videoRef.current.srcObject = stream;
+                                    videoRef.current.autofocus = true;
+                                    videoRef.current.playsInline = true; // Fix error in Safari
+                                    videoRef.current.play().then(() => {
+                                        codeReaderRef.current!.decodeOnce(videoRef.current!).then(result => {
+                                            setResultsMap(oldResults => {
+                                                if (oldResults.find(old => old.id === result.getText())) {
+                                                    const changedResult = oldResults.slice(0);
+                                                    let indexToChange: number | undefined;
+                                                    changedResult.forEach((element, index) => {
+                                                        if (element.id === result.getText()) {
+                                                            indexToChange = index;
+                                                        }
+                                                    });
+                                                    if (indexToChange !== undefined) {
+                                                        changedResult[indexToChange] = {
+                                                            id: result.getText(),
+                                                            result: result.getResultPoints()
+                                                        };
+                                                    }
+                                                    return changedResult;
+                                                } else {
+                                                    const newArray = [
+                                                        ...oldResults,
+                                                        { id: result.getText(), result: result.getResultPoints() }
+                                                    ];
+                                                    return newArray;
                                                 }
                                             });
-                                            if (indexToChange !== undefined) {
-                                                changedResult[indexToChange] = {
-                                                    id: result.getText(),
-                                                    result: result.getResultPoints()
-                                                };
-                                            }
-                                            return changedResult;
-                                        } else {
-                                            const newArray = [
-                                                ...oldResults,
-                                                { id: result.getText(), result: result.getResultPoints() }
-                                            ];
-                                            return newArray;
-                                        }
+
+                                            codeReaderRef.current?.stopAsyncDecode();
+                                            codeReaderRef.current?.reset();
+                                            if (videoRef.current) videoRef.current.pause();
+                                            stream.getVideoTracks().forEach(track => track.stop());
+                                            stopped.current = true;
+                                        });
                                     });
-                                    
-                                    codeReaderRef.current?.stopAsyncDecode();
-                                    codeReaderRef.current?.reset();
-                                    if(videoRef.current) videoRef.current.pause();
-                                    stream.getVideoTracks().forEach(track => track.stop());
-                                    stopped.current = true;
+                                }
                             })
-                            })
-                        }
-                        
-                    }).catch(reason => {
-                        console.log("catch: " + reason)
+                            .catch(reason => {
+                                console.log("catch: " + reason);
+                                stopped.current = true;
+                            });
+                    } catch (error) {
+                        console.log(error);
                         stopped.current = true;
-                    })
-                } catch (error) {
-                    console.log(error)
-                    stopped.current = true;
+                    }
                 }
-                }
-            }
-            engineContext.scene.onPointerObservable.add((pointerInfo) => {
+            };
+            engineContext.scene.onPointerObservable.add(pointerInfo => {
                 switch (pointerInfo.type) {
-                  case PointerEventTypes.POINTERDOWN:
-                    scan();
-                    break;
+                    case PointerEventTypes.POINTERDOWN:
+                        scan();
+                        break;
                 }
             });
             setObservableSet(true);
         }
-    }, [engineContext.scene, codeReaderRef.current, videoRef.current])
+    }, [engineContext.scene, codeReaderRef.current, videoRef.current]);
 
     useEffect(() => {
         resultsMap.forEach(result => {
@@ -156,16 +147,19 @@ export function WebARImageTracker(props: WebARImageTrackerContainerProps): React
             if (engineContext.scene) {
                 var rays: Ray[] = [];
                 result.result.forEach((point, index) => {
-                    const ray = engineContext.scene!.pick(point.getX(), point.getY())
-                    if(ray?.ray){
+                    const ray = engineContext.scene!.pick(point.getX(), point.getY());
+                    if (ray?.ray) {
                         rays[index] = ray.ray;
                     }
                 });
                 if (rays.length !== 0) {
                     if (foundIndex !== -1 && cubes[foundIndex].mesh) {
                         rays.forEach((currentRay, index) => {
-                            const closestPoint =  ClosestPointOnTwoLines( cubes[foundIndex].previousRays[index], currentRay);
-                            if(closestPoint !== Vector3.Zero()) {
+                            const closestPoint = ClosestPointOnTwoLines(
+                                cubes[foundIndex].previousRays[index],
+                                currentRay
+                            );
+                            if (closestPoint !== Vector3.Zero()) {
                                 setCubes(oldcubes => {
                                     oldcubes[foundIndex].mesh.position = closestPoint;
                                     oldcubes[foundIndex].previousRays[index] = currentRay;
@@ -197,7 +191,7 @@ export function WebARImageTracker(props: WebARImageTrackerContainerProps): React
 
     return (
         <>
-        <canvas ref={canvasRef} width="300" height="300"/>
+            <canvas ref={canvasRef} width="300" height="300" />
             <ParentContext.Provider value={parentID}>{props.mxContentWidget}</ParentContext.Provider>
         </>
     );
