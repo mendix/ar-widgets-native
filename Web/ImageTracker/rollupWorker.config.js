54cd71ef2141b12ce7ae0ebb564f5ac05556093f
hedwig.doets <hedwig.doets@mendix.com>
2023-06-20 09:39:08 +0200

    Remove comments

--- Web/ImageTracker/rollupWorker.config.js
+++ Web/ImageTracker/rollupWorker.config.js
@@ -1,9 +1,6 @@
 import { getBabelOutputPlugin } from "@rollup/plugin-babel";
-// require("@rollup/amd-loader");
 import resolve from "@rollup/plugin-node-resolve";
-import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
-import analyze from "rollup-plugin-analyzer";
 import bundleSize from "rollup-plugin-bundle-size";
 
 export default {
@@ -11,29 +8,18 @@ export default {
     output: {
         format: "umd",
         file: "./src/bundle/Worker.js",
-        // paths: {"@zxing/library/cjs": "../../../shared/zxinglibrary.js"}
     },
 
     plugins: [
         typescript(),
         resolve(),
-        // analyze({ summaryOnly: true, limit: 20 }),
-   
         getBabelOutputPlugin({
             allowAllFormats: true,
             babelrc: false,
-            // Disable compact output to keep comments
             compact: false,
-            // Keep all comments (terser will process them).
             shouldPrintComment: () => true,
             presets: [["@babel/preset-env", { targets: { safari: "12" } }]]
         }),
-        
-        // terser({
-        //     output: {
-        //         comments: /@preserve|@?copyright|@lic|@cc_on|licen[cs]e|^\**!/i
-        //     }
-        // }),
         bundleSize()
     ]
 };
