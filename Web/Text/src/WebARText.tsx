622711156c2db6387acb6b1542a32695fb1f74b3
hedwig.doets <hedwig.doets@mendix.com>
2023-10-30 12:16:50 +0100

    Use prop destructuring, combine useeffects

--- Web/Text/src/WebARText.tsx
+++ Web/Text/src/WebARText.tsx
@@ -8,7 +8,7 @@ import earcut from "earcut";
 
 export function WebARText(props: WebARTextContainerProps): React.ReactElement {
     (window as any).earcut = earcut;
-    const { mxMaterialTexture } = props;
+    const { mxMaterialTexture, mxText, name, tabIndex, style, ...restOfProps } = props;
     const [mesh, setMesh] = useState<Mesh>();
     const [scene, setScene] = useState<Scene>();
     const gizmoTransform = useGizmoComponent({
@@ -21,7 +21,7 @@ export function WebARText(props: WebARTextContainerProps): React.ReactElement {
     });
 
     const handleSceneLoaded = (scene: Scene) => {
-        const newMesh = MeshBuilder.CreateText("myText", props.mxText.value ?? "", font, {
+        const newMesh = MeshBuilder.CreateText("myText", mxText.value ?? "", font, {
             resolution: 64,
             depth: 10
         });
@@ -37,19 +37,13 @@ export function WebARText(props: WebARTextContainerProps): React.ReactElement {
         if (gizmoTransform.newPosition) {
             setAttributes(gizmoTransform.newPosition, props.mxPositionXAtt, props.mxPositionYAtt, props.mxPositionZAtt);
         }
-    }, [gizmoTransform.newPosition]);
-
-    useEffect(() => {
         if (gizmoTransform.newRotation) {
             setAttributes(gizmoTransform.newRotation, props.mxRotationXAtt, props.mxRotationYAtt, props.mxRotationZAtt);
         }
-    }, [gizmoTransform.newRotation]);
-
-    useEffect(() => {
         if (gizmoTransform.newScale) {
             setAttributes(gizmoTransform.newScale, props.mxScaleXAtt, props.mxScaleYAtt, props.mxScaleZAtt);
         }
-    }, [gizmoTransform.newScale]);
+    }, [gizmoTransform.newPosition, gizmoTransform.newRotation, gizmoTransform.newScale]);
 
     useEffect(() => {
         if (mxMaterialTexture !== undefined && scene) {
@@ -63,69 +57,27 @@ export function WebARText(props: WebARTextContainerProps): React.ReactElement {
     }, [mxMaterialTexture, scene]);
 
     useEffect(() => {
-        if (props.mxText.value && font !== undefined) {
+        if (mxText.value && font !== undefined) {
             mesh?.dispose();
-            const newMesh = MeshBuilder.CreateText("myText", props.mxText.value ?? "", font, {
+            const newMesh = MeshBuilder.CreateText("myText", mxText.value ?? "", font, {
                 resolution: 64,
                 depth: 10
             });
             if (newMesh) setMesh(newMesh);
         }
-    }, [props.mxText, font]);
+    }, [mxText, font]);
 
     return (
         <MeshComponent
+            {...restOfProps}
             rootMesh={mesh}
             allMeshes={mesh ? [mesh] : undefined}
-            mxPositionType={props.mxPositionType}
-            mxPositionXStat={props.mxPositionXStat}
-            mxPositionYStat={props.mxPositionYStat}
-            mxPositionZStat={props.mxPositionZStat}
-            mxPositionYAtt={props.mxPositionYAtt}
-            mxPositionXAtt={props.mxPositionXAtt}
-            mxPositionZAtt={props.mxPositionZAtt}
-            mxPositionXExpr={props.mxPositionXExpr}
-            mxPositionYExpr={props.mxPositionYExpr}
-            mxPositionZExpr={props.mxPositionZExpr}
-            mxRotationType={props.mxRotationType}
-            mxRotationXStat={props.mxRotationXStat}
-            mxRotationYStat={props.mxRotationYStat}
-            mxRotationZStat={props.mxRotationZStat}
-            mxRotationXAtt={props.mxRotationXAtt}
-            mxRotationYAtt={props.mxRotationYAtt}
-            mxRotationZAtt={props.mxRotationZAtt}
-            mxRotationXExpr={props.mxRotationXExpr}
-            mxRotationYExpr={props.mxRotationYExpr}
-            mxRotationZExpr={props.mxRotationZExpr}
-            mxScaleType={props.mxScaleType}
-            mxScaleXStat={props.mxScaleXStat}
-            mxScaleYStat={props.mxScaleYStat}
-            mxScaleZStat={props.mxScaleZStat}
-            mxScaleXAtt={props.mxScaleXAtt}
-            mxScaleYAtt={props.mxScaleYAtt}
-            mxScaleZAtt={props.mxScaleZAtt}
-            mxScaleXExpr={props.mxScaleXExpr}
-            mxScaleYExpr={props.mxScaleYExpr}
-            mxScaleZExpr={props.mxScaleZExpr}
             OnSceneLoaded={handleSceneLoaded}
             mxMaterialColor={props.mxMaterialColor.value ?? "#0CABF9"}
-            mxMaterialOption={props.mxMaterialOption}
             texture={texture}
-            mxMetalness={props.mxMetalness}
-            mxOpacity={props.mxOpacity}
-            mxRoughness={props.mxRoughness}
-            mxLightingType={props.mxLightingType}
-            mxUseDraggingInteraction={props.mxUseDraggingInteraction}
-            mxUsePinchInteraction={props.mxUsePinchInteraction}
             mxDraggingEnabled={props.mxDraggingEnabled.value ?? false}
-            mxDragType={props.mxDragType}
-            mxOnDrag={props.mxOnDrag}
             mxPinchEnabled={props.mxPinchEnabled.value ?? false}
             mxPinchToScaleEnabled={false}
-            mxOnPinchActionValue={props.mxOnPinchActionValue}
-            mxOnHoverEnter={props.mxOnHoverEnter}
-            mxOnHoverExit={props.mxOnHoverExit}
-            mxOnClick={props.mxOnClick}
         />
     );
 }
