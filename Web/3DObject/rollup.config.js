e377bd4cb7597ccbd2612e2babcf5aabb94a0a9f
hedwig.doets <hedwig.doets@mendix.com>
2024-02-22 12:50:47 +0100

    Undo changes to 3d object rollup

--- Web/3DObject/rollup.config.js
+++ Web/3DObject/rollup.config.js
@@ -1,3 +1,37 @@
-import defaultConfig from "../rollup.config";
+import typescript from "@rollup/plugin-typescript";
+import fs from "fs";
 
-export default defaultConfig;
+export default args => {
+    const result = args.configDefaultConfig;
+    result.treeshake = false;
+    result.forEach(config => {
+        const external = [/^@babylonjs\/core($|\/)/];
+        config.external = [...config.external, ...external];
+        config.plugins.push(
+            typescript({
+                include: ["../../Shared/ComponentParent/**/*.ts+(|x)", "./**/*.ts+(|x)"]
+            }),
+            {
+                writeBundle(bundle) {
+                    for (const [fileName, chunkOrAsset] of Object.entries(bundle)) {
+                        if (
+                            chunkOrAsset !== undefined &&
+                            (chunkOrAsset.toString().endsWith(".js") || chunkOrAsset.toString().endsWith(".mjs")) &&
+                            fileName === "file"
+                        ) {
+                            const data = fs.readFileSync(chunkOrAsset, { encoding: "utf8" });
+                            const regex = /@babylonjs\/core\/.*?\.js/g;
+                            fs.writeFileSync(chunkOrAsset, data.replaceAll(regex, "../../../shared/babylonjscore"));
+                        }
+                    }
+                }
+            }
+        );
+        config.output.paths = {
+            ...config.output.paths,
+            "@babylonjs/core": "../../../shared/babylonjscore.js"
+        };
+    });
+
+    return result;
+};
