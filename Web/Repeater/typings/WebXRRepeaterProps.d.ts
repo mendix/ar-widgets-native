ca48be0088728b2f5fbcdbcb327ef92389bb2dbd
hedwig.doets <hedwig.doets@mendix.com>
2024-03-01 14:51:56 +0100

    Changed props web repeater

--- Web/Repeater/typings/WebXRRepeaterProps.d.ts
+++ Web/Repeater/typings/WebXRRepeaterProps.d.ts
@@ -7,26 +7,23 @@ import { ComponentType, CSSProperties, ReactNode } from "react";
 import { ListValue, ListWidgetValue } from "mendix";
 
 export interface WebXRRepeaterContainerProps {
-  name: string;
-  class: string;
-  style?: CSSProperties;
-  tabIndex?: number;
-  datasource: ListValue;
-  content: ListWidgetValue;
+    name: string;
+    class: string;
+    style?: CSSProperties;
+    tabIndex?: number;
+    datasource: ListValue;
+    content: ListWidgetValue;
 }
 
 export interface WebXRRepeaterPreviewProps {
-  /**
-   * @deprecated Deprecated since version 9.18.0. Please use class property instead.
-   */
-  className: string;
-  class: string;
-  style: string;
-  styleObject?: CSSProperties;
-  readOnly: boolean;
-  datasource: {} | { caption: string } | { type: string } | null;
-  content: {
-    widgetCount: number;
-    renderer: ComponentType<{ children: ReactNode; caption?: string }>;
-  };
+    /**
+     * @deprecated Deprecated since version 9.18.0. Please use class property instead.
+     */
+    className: string;
+    class: string;
+    style: string;
+    styleObject?: CSSProperties;
+    readOnly: boolean;
+    datasource: {} | { caption: string } | { type: string } | null;
+    content: { widgetCount: number; renderer: ComponentType<{ children: ReactNode; caption?: string }> };
 }
