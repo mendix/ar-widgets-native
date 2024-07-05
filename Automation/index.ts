9e190c5642f2c6a39bd08f5af999a6d7edb0dbdc
hedwig.doets <hedwig.doets@mendix.com>
2024-07-05 13:36:06 +0200

    Add check for web in automation

--- Automation/index.ts
+++ Automation/index.ts
@@ -2,12 +2,16 @@ import { createDraft, publishDraft } from "./api/contributor";
 import { Version } from "./api/utils/version";
 
 function CreateDraftInMarketplace() {
+    if (process.env.MODULE_VERSION?.startsWith("web")) {
+        console.error("release tag for something other than web, currently not implemented");
+        return;
+    }
+
     const version = process.env.MODULE_VERSION?.split("v").pop();
     if (version === undefined) {
         console.error("No version in tag: " + process.env.MODULE_VERSION);
         return;
     }
-
     const appversion: Version = Version.fromString(version);
     const studioProVersion: Version = new Version(9, 21, 2);
     const result = createDraft({
