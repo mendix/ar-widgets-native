44706e832b05eb2e0df0e219de16e3ffb2186c1a
hedwig.doets <hedwig.doets@mendix.com>
2024-07-05 10:47:02 +0200

    uncomment create and publish draft

--- Automation/index.ts
+++ Automation/index.ts
@@ -2,23 +2,28 @@ import { createDraft, publishDraft } from "./api/contributor";
 import { Version } from "./api/utils/version";
 
 function CreateDraftInMarketplace() {
-    console.log(process.env.MODULE_VERSION);
-    // const appversion: Version = new Version(0, 0, 3);
-    // const studioProVersion: Version = new Version(9, 21, 2);
-    // const result = createDraft({
-    //     appName: "WebXR Widgets",
-    //     appNumber: 203481, //number in marketplace
-    //     version: appversion,
-    //     studioProVersion: studioProVersion,
-    //     artifactUrl: "https://github.com/HedwigAR/ar-widgets-native/releases/download/web-v0.0.3/WebXRWidgets.mpk" //link to github mpk
-    // });
-    // result.then(success => {
-    //     console.log(success);
-    //     publishDraft({ draftUUID: success.UUID });
-    // });
-    // result.catch(rejected => {
-    //     console.log(rejected);
-    // });
+    const version = process.env.MODULE_VERSION?.split("v").pop();
+    if (version === undefined) {
+        console.error("No version in tag: " + process.env.MODULE_VERSION);
+        return;
+    }
+
+    const appversion: Version = Version.fromString(version);
+    const studioProVersion: Version = new Version(9, 21, 2);
+    const result = createDraft({
+        appName: "WebXR Widgets",
+        appNumber: 203481, //number in marketplace
+        version: appversion,
+        studioProVersion: studioProVersion,
+        artifactUrl: `https://github.com/HedwigAR/ar-widgets-native/releases/download/${process.env.MODULE_VERSION}/WebXRWidgets.mpk` //link to github mpk
+    });
+    result.then(success => {
+        console.log(success);
+        publishDraft({ draftUUID: success.UUID });
+    });
+    result.catch(rejected => {
+        console.log(rejected);
+    });
 }
 
 CreateDraftInMarketplace();
